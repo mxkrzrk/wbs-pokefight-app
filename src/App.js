3084edd94e2fda76a502f24ccedbf7c1c0b3777e
Massimilianok <massimiliano.rizzuto87@gmail.com>
2021-03-12 16:51:47 +0100

    feat: Add routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Switch, Route, Redirect } from 'react-router';
+import './App.css';
 import Header from './components/Header/Header';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -7,6 +9,19 @@ function App() {
     <Container fluid>
       <Row>
         <Header />
+      </Row>
+      <Row>
+        <main className="main">
+          <Switch>
+            <Route path="/" exact>
+              <div>All preview cards</div>
+            </Route>
+            <Route path="/pokemon/:id" exact>
+              <div>Single info card</div>
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        </main>
       </Row>
     </Container>
   );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap-reboot.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
@@ -8,7 +9,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
