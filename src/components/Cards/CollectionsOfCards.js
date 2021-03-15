b5886bb05e63ecf8b485bcefff56d66bae479e42
Giulia Giovannoli <giovannoli.giulia@gmail.com>
2021-03-15 20:44:25 +0100

    fight

--- src/components/Cards/CollectionsOfCards.js
+++ src/components/Cards/CollectionsOfCards.js
@@ -1,21 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import './Cards.css';
+import { useParams } from 'react-router-dom';
+import './cards.css';
 import Cards from './Cards';
 import Col from 'react-bootstrap/Col'
 
 export default function Collections() {
+
+  const { name } = useParams()
   const [pokemon, setPokemon] = useState();
 
   useEffect(() => {
-    fetch('http://localhost:8080/api/pokemon')
+    fetch("https://pokeapi.co/api/v2/pokemon")
       .then((res) => res.json())
-      .then((data) => setPokemon(data.data))
+      .then((data) => setPokemon(data.results))
       .catch((e) => console.log(e.message));
   }, []);
 
   return (
     <Col className="d-flex flex-column align-items-center flex-md-row flex-md-wrap justify-content-center">
-      {pokemon &&
+    
+    {pokemon && 
         pokemon.map((cat) => {
           return <Cards key={cat.id} cat={cat} />;
         })}
