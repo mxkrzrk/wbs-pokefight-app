f0236a7705cac8ad73f9aaab4f50a5fbfc014dfa
Massimilianok <massimiliano.rizzuto87@gmail.com>
2021-03-17 09:56:10 +0100

    feat: fetch next data with infinite scrolling

--- src/components/Cards/CollectionsOfCards.js
+++ src/components/Cards/CollectionsOfCards.js
@@ -4,25 +4,50 @@ import Cards from './Cards';
 import Col from 'react-bootstrap/Col';
 
 export default function Collections() {
-  const [pokemon, setPokemon] = useState();
+  const [pokemon, setPokemon] = useState({
+    prev: null,
+    next: null,
+    pokemon: [],
+    url: 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=20',
+  });
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon')
-      .then((res) => res.json())
-      .then((data) => setPokemon(data.results))
-      .catch((e) => console.log(e.message));
+    window.addEventListener('scroll', scrollPositionHandle);
+    return () => window.removeEventListener('scroll', scrollPositionHandle);
   }, []);
 
+  useEffect(() => {
+    fetch(pokemon.url)
+      .then((res) => res.json())
+      .then((data) => {
+        setPokemon((prevState) => ({
+          ...prevState,
+          prev: data.previous,
+          next: data.next,
+          pokemon: prevState.pokemon.concat(data.results),
+        }));
+      })
+      .catch((e) => console.log(e.message));
+  }, [pokemon.url]);
+
+  const scrollPositionHandle = () => {
+    if (
+      Math.round(window.innerHeight + window.scrollY) ===
+      document.body.scrollHeight
+    ) {
+      setPokemon((prevState) => ({
+        ...prevState,
+        url: prevState.next,
+      }));
+    }
+  };
+
   return (
-    <>
-      {pokemon && (
-        <Col className="d-flex flex-column align-items-center flex-md-row flex-md-wrap justify-content-center">
-          {pokemon &&
-            pokemon.map((poke, index) => {
-              return <Cards key={index} {...poke} />;
-            })}
-        </Col>
-      )}
-    </>
+    <Col className="d-flex flex-column align-items-center flex-md-row flex-md-wrap justify-content-center">
+      {pokemon.pokemon &&
+        pokemon.pokemon.map((poke, index) => {
+          return <Cards key={index} {...poke} />;
+        })}
+    </Col>
   );
 }
